2fa4759cce5788f4136832ccc1cd7f665fb98357
JohnsonZhaoEX <10162073@mail.ecust.edu.cn>
2017-12-25 11:49:35 +0800

    传输文件内的文字并保存

--- client.py
+++ client.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python
 import socket
 import os
+import sys
 ss=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 ss.connect(('127.0.0.2',7500))
-#f=open('aa','wb')
-ss.sendall('Your text here')
+re = os.open("Content.txt", os.O_RDONLY)
+
+'''string = raw_input()
+print string
+w = os.write(re,string)'''
+
+f = os.read(re,100)
+ss.sendall(f)
+os.close(re)
 os.system('sleep 1')
 ss.send('FOE')
 data=ss.recv(1024)
--- server.py
+++ server.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python
 
 import socket
+import sys
+import os
 host ='127.0.0.2'
 port =7500
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.bind((host,port))
 s.listen(2)
+result=os.open('output.txt',os.O_RDWR|os.O_CREAT)
 try:
 	while True:
 		conn,add=s.accept()
 		while True:
 			data2=''
-			data1=conn.recv(3)
+			data1=conn.recv(100)
 			if data1=='EOF':
 				conn.send('hello client1')
 				break
@@ -19,7 +22,10 @@ try:
 				conn.send('hello client2')
 				break
 			data2+=data1
+			os.write(result,data2)
 			print data1
+#	sys.stdout=fd
 except KeyboardInterrupt:
 	print "you have CTRL+C,Now quit"
+	os.close(result)
 	s.close()
